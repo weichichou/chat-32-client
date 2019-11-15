92e264f68713b42bf955fa7b070ce28521857e24
weichichou <weichi.cc@gmail.com>
2019-11-15 12:04:06 +0100

    Render messages on the page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,41 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    messages: ['fake']
+  }
+
   stream = new EventSource('http://localhost:4000/stream')
 
   componentDidMount(){
-    this.stream.onmessage = function(event){
+    this.stream.onmessage = (event) => {
       const {data} = event
-      console.log('data test:', data)
+
+      // convert the string back to javascript
+      const parsed = JSON.parse(data)
+
+      if (Array.isArray(parsed)){
+        this.setState({messages: parsed})
+      }else{
+        const messages = [
+          ...this.state.messages, parsed
+        ]
+        this.setState({messages})
+      }
+
+      console.log('parsed test:', parsed)
     }
   }
 
   render(){
+    const list = this.state.messages
+      .map((message, index) => <p key={index}>{message}</p>)
     return (
       <div className="App">
         <h1>Hello World</h1>
+        <div>
+          {list}
+        </div>
       </div>
     )
   }
