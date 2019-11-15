9efe488c98130925c680daa5066a916203a8dbfb
weichichou <weichi.cc@gmail.com>
2019-11-15 11:31:10 +0100

    Connect client and server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  stream = new EventSource('http://localhost:4000/stream')
+
+  componentDidMount(){
+    this.stream.onmessage = function(event){
+      const {data} = event
+      console.log('data test:', data)
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
